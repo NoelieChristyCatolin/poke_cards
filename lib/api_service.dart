0e0a6e4000d1d7418e5d33fb16a7060bbd8a4e2e
noeliechristycatolin <noeliechristycatolin@gmail.com>
2021-11-26 22:49:33 +0800

    added api service

---
+++ lib/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  Future<Response> fetchPokemonDetails(String pokemonName) async{
+    try{
+      var response = await Dio().post('https://pokeapi.co/api/v2/pokemon/$pokemonName');
+      return response;
+    }
+    catch(e) {
+      throw Exception('Failed to fetch');
+    }
+  }
+}
